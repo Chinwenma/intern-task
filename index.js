87aeeaa1b7765b5d69542023c9e11741875e0a85
Chinwenm Okorie <okoriechinwe1327@gmail.com>
2025-08-09 09:13:04 +0100

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+// 2. How to Include JavaScript in HTML
+// There are three common ways to include JS in HTML:
+// a. Inline JavaScript: inside the opening tag of an html element)
+//b.  internal javascript(usining script tag <script></script> tag usually placed before the closing body tag element. This is also to avoid slowing down page rendering)
+// c.  external javascript: You can also create a file with .js extenstion then link it back to your html file using the script:src tag <script src=""></script>
+
+// script.js
+// alert("External JavaScript Loaded!");
+
